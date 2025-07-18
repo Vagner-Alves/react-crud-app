e8d9d5c9de4f6ce1db0f2c3c31d2bb1e48a32487
Vagner-Alves <vagneralves997@gmail.com>
2025-07-17 22:28:56 -0300

    bug fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Gerenciador de Usuarios/i);
   expect(linkElement).toBeInTheDocument();
 });
